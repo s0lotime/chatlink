c0cb1a7782070f8ed378eccfffe834d985cff3be
solo <113215526+s0lotime@users.noreply.github.com>
2025-04-26 22:52:27 -0400

    Update chatlink.js

--- cdn/js/chatlink.js
+++ cdn/js/chatlink.js
@@ -20,7 +20,7 @@ async function loadPriorMessages() {
 		return;
 	}
 	data.forEach(msg => {
-		addMessage(msg.content);
+		receiveMessage(msg.content);
 	});
 }
 
@@ -59,7 +59,7 @@ async function startRealtime() {
 				table: 'messages'
 			}, (payload) => {
 				console.log('New message!', payload.new);
-				addMessage(payload.new.content || JSON.stringify(payload.new));
+				(payload.new.content || JSON.stringify(payload.new));
 			})
 			.subscribe();
 		console.log("connected");
